4cf78a5f09bee6f5c9aaca1acfb3ffac9be81a4c
Joeknow03 <jahongirakiljanov@gmail.com>
2024-04-17 19:28:01 +0300

    pls6

--- test_student_service.py
+++ test_student_service.py
@@ -3,49 +3,25 @@ import requests
 
 def test_get_all_companies(url: str):
     res = requests.get(url).json()
-    assert(res == [{'student_id': 1,
-             'name': 'John Smith',
-             'gender': 'Male',
-             'age': 20,
-             'learning': 'Computer Science',
-             'city': 'New York'},
-            {'student_id': 2,
-             'name': 'Emma Johnson',
-             'gender': 'Female',
-             'age': 21,
-             'learning': 'Biology',
-             'city': 'Los Angeles'},
-            {'student_id': 3,
-             'name': 'Michael Lee',
-             'gender': 'Male',
-             'age': 19,
-             'learning': 'History',
-             'city': 'Chicago'},
-            {'student_id': 4,
-             'name': 'Sophia Garcia',
-             'gender': 'Female',
-             'age': 22,
-             'learning': 'Psychology',
-             'city': 'London'},
-            {'student_id': 5,
-             'name': 'Daniel Kim',
-             'gender': 'Male',
-             'age': 20,
-             'learning': 'Engineering',
-             'city': 'Tokyo'}])
+    assert (res == [{'student_id': 1, 'name': 'John Smith', 'gender': 'Male', 'age': 20, 'learning': 'Computer Science',
+                     'city': 'New York'},
+                    {'student_id': 2, 'name': 'Emma Johnson', 'gender': 'Female', 'age': 21, 'learning': 'Biology',
+                     'city': 'Los Angeles'},
+                    {'student_id': 3, 'name': 'Michael Lee', 'gender': 'Male', 'age': 19, 'learning': 'History',
+                     'city': 'Chicago'},
+                    {'student_id': 4, 'name': 'Sophia Garcia', 'gender': 'Female', 'age': 22, 'learning': 'Psychology',
+                     'city': 'London'},
+                    {'student_id': 5, 'name': 'Daniel Kim', 'gender': 'Male', 'age': 20, 'learning': 'Engineering',
+                     'city': 'Tokyo'}])
 
 
 def test_get_company_by_id(url: str):
     res = requests.get(url).json()
-    assert(res == {'student_id': 1,
-             'name': 'John Smith',
-             'gender': 'Male',
-             'age': 20,
-             'learning': 'Computer Science',
-             'city': 'New York'})
+    assert (res == {'student_id': 1, 'name': 'John Smith', 'gender': 'Male', 'age': 20, 'learning': 'Computer Science',
+                    'city': 'New York'})
 
 
 if __name__ == '__main__':
-    URL = 'http://127.0.0.1:79/api/v1/students/'
+    URL = 'http://127.0.0.1:80/api/v1/students/'
     test_get_company_by_id(URL + '1')
     test_get_all_companies(URL)
